5b1ae49e8489885e70a7a653116d0552f61baf6c
veerandrakumark <kit28.24bcb058@gmail.com>
2025-12-19 15:40:33 +0530

    Added Custom Query  Method

--- src/main/java/com/ecommerce/respository/UserRespository.java
+++ src/main/java/com/ecommerce/respository/UserRespository.java
@@ -1,15 +1,34 @@
+//Queries are defined in repository interfaces using method names or @Query annotations.
+//Jpa give inbuilt functions only for CRUD operations(Inbuilt queries Create,Read,Update,Delete)..
+// for complex queries we have to define our own methods.
+
+
+
+
+
 package com.ecommerce.respository;
 import org.spingframework.data.jpa.respository.JpaRepository;
 import com.ecommerce.entity.User;
 public class UserRespository extends JpaRepository<User, Long> { //JpaRepository is a JPA specific extension of Repository. It contains the full API of CrudRepository and PagingAndSortingRepository.
                                                               //It provides JPA related methods such as flushing the persistence context and delete records in a batch.
                                                               // User Long means the entity type is User and the primary key type is Long.
-    Optional<User> findByUsername(String username); //If no user ,it will return null ...It will handles the nullPointer exception since 
-    Optional<User> findByEmail(String email);//In user u1; there is a null...it doesn't throw error...instead it return null...
-    boolean existsByUsername(String username); //If give User alone it may throw error...this function replaces the query..
-    boolean existsByeEmail(String email);
 
-    @Query("SELECT u FROM User u WHERE u.username = :username OR u.email = :email") //JPQL query to find user by username or email
-    Optional<User> findByUsernameOrEmail(@Param("username")String username,@Param("email")String email); //It will search for user by username or email
+    // findByUsername(String username) -> Select * from users where username = ?
+    // It should be public and return one user object or Optional<User>(After java 8 introduced,in util package) to handle null values.
+    public Optional<User> findByUsername(String username); //If no user ,it will return null ...It will handles the nullPointer exception since 
+    public Optional<User> findByEmail(String email);       //In user u1(without new its not an object just a var); there is a null...it doesn't throw error...instead it return null...
+   
+    //existsByUsername(String username) -> Select count(*) > 0 from users where username = ?
+    // It should be public and return boolean value.
+    public boolean existsByUsername(String username); //If give User alone it may throw error...this function replaces the query..
+    public boolean existsByEmail(String email);
+    
+
+    //Custom query using @Query annotation
+    //If more than 3 words exists we have to use @Query annotation to define our own query.
+    //each user is considered as u... 
+    //first row is u, u.username andha row la iruka username
+    @Query("SELECT u FROM User u WHERE u.username =?1 OR u.email = ?2")  
+    Optional<User> findByUsernameOrEmail(String username,String email); //It will search for user by username or email
 
 }
